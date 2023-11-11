d5ab9014076460a21e7abaef6fc82beb73ab039b
Asim Thaha <iamasimthaha@gmail.com>
2023-11-12 00:02:30 +0530

    made data dynamic using api

--- src/components/ToDoView.jsx
+++ src/components/ToDoView.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ToDoNavbar from "./ToDoNavbar";
+import axios from "axios";
 
 const ToDoView = () => {
+  const [data, changeData] = useState([]);
+
+  const fetchData = () => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then((response) => {
+      changeData(response.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
       <ToDoNavbar />
@@ -9,123 +22,19 @@ const ToDoView = () => {
         <div className="row">
           <div className="col">
             <div className="row g-3">
-              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                <div class="card text-center">
-                  <div class="card-header">To Do Title</div>
-                  <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">
-                      With supporting text below as a natural lead-in to
-                      additional content.
-                    </p>
+              {data.map((value, index) => {
+                return (
+                  <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                    <div class="card text-center">
+                      <div class="card-header">{value.title}</div>
+                      <div class="card-body">
+                        <p class="card-text">{value.completed}</p>
+                      </div>
+                      <div class="card-footer text-muted">{value.userId}</div>
+                    </div>
                   </div>
-                  <div class="card-footer text-muted">2 days ago</div>
-                </div>
-              </div>
-              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                <div class="card text-center">
-                  <div class="card-header">To Do Title</div>
-                  <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">
-                      With supporting text below as a natural lead-in to
-                      additional content.
-                    </p>
-                  </div>
-                  <div class="card-footer text-muted">2 days ago</div>
-                </div>
-              </div>
-              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                <div class="card text-center">
-                  <div class="card-header">To Do Title</div>
-                  <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">
-                      With supporting text below as a natural lead-in to
-                      additional content.
-                    </p>
-                  </div>
-                  <div class="card-footer text-muted">2 days ago</div>
-                </div>
-              </div>
-              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                <div class="card text-center">
-                  <div class="card-header">To Do Title</div>
-                  <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">
-                      With supporting text below as a natural lead-in to
-                      additional content.
-                    </p>
-                  </div>
-                  <div class="card-footer text-muted">2 days ago</div>
-                </div>
-              </div>
-              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                <div class="card text-center">
-                  <div class="card-header">To Do Title</div>
-                  <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">
-                      With supporting text below as a natural lead-in to
-                      additional content.
-                    </p>
-                  </div>
-                  <div class="card-footer text-muted">2 days ago</div>
-                </div>
-              </div>
-              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                <div class="card text-center">
-                  <div class="card-header">To Do Title</div>
-                  <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">
-                      With supporting text below as a natural lead-in to
-                      additional content.
-                    </p>
-                  </div>
-                  <div class="card-footer text-muted">2 days ago</div>
-                </div>
-              </div>
-              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                <div class="card text-center">
-                  <div class="card-header">To Do Title</div>
-                  <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">
-                      With supporting text below as a natural lead-in to
-                      additional content.
-                    </p>
-                  </div>
-                  <div class="card-footer text-muted">2 days ago</div>
-                </div>
-              </div>
-              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                <div class="card text-center">
-                  <div class="card-header">To Do Title</div>
-                  <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">
-                      With supporting text below as a natural lead-in to
-                      additional content.
-                    </p>
-                  </div>
-                  <div class="card-footer text-muted">2 days ago</div>
-                </div>
-              </div>
-              <div className="col col-12 col-sm-6 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
-                <div class="card text-center">
-                  <div class="card-header">To Do Title</div>
-                  <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">
-                      With supporting text below as a natural lead-in to
-                      additional content.
-                    </p>
-                  </div>
-                  <div class="card-footer text-muted">2 days ago</div>
-                </div>
-              </div>
+                );
+              })}
             </div>
           </div>
         </div>
